f38364944308be57e1afdf770a74b5100f1c4cbc
Manuele D'Ambrosio <manuele.dambrosio@studio.unibo.it>
2025-05-15 14:24:28 +0200

    Obtained descrete results with LSTM Classification

--- ml_utils_classification.py
+++ ml_utils_classification.py
@@ -5,3 +5,10 @@ def split_before_year(X, y, year):
     X_val = X.loc[~is_train]
     y_val = y.loc[~is_train]
     return X_train, X_val, y_train, y_val
+
+def direction_momentum(df, horizons=[5, 10, 20, 60]):
+    new_df = df.copy()
+    for horizon in horizons:
+        name = f"direction_mom_{horizon}"
+        new_df[name] = new_df['direction'].rolling(horizon).sum() - (horizon/2)
+    return new_df
